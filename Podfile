05b252dd7505476a00364c847b91ed18b887cea3
Bruno <brunoteixeiralc@gmail.com>
2018-03-18 20:06:19 -0300

    commit - fastlane

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+
+target 'meuslugares' do
+  use_frameworks!
+
+  pod 'lottie-ios'
+
+end
---
+++ meuslugaresUITests/meuslugaresUITests.swift
@@ -0,0 +1,25 @@
+//
+//  meuslugaresUITests.swift
+//  meuslugaresUITests
+//
+//  Created by Bruno Corrêa on 18/03/2018.
+//  Copyright © 2018 Bruno Lemgruber. All rights reserved.
+//
+
+import XCTest
+
+class meuslugaresUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+    }
+    
+}
