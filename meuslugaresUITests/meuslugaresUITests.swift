fc5b4bbcadf072419f595e4e7a8ac061a182090e
Bruno <brunoteixeiralc@gmail.com>
2018-03-27 23:08:27 -0300

    commit - config to itunes connect

--- meuslugaresUITests/meuslugaresUITests.swift
+++ meuslugaresUITests/meuslugaresUITests.swift
@@ -24,43 +24,11 @@ class meuslugaresUITests: XCTestCase {
     
     func testUI() {
         
-        let app = XCUIApplication()
-        snapshot("Tela Principal")
-        
-        let goButton = app.buttons["Marcar minha localização"]
-        let exists = NSPredicate(format: "exists == true")
-        expectation(for: exists, evaluatedWith: goButton, handler: nil)
-        
-        addUIInterruptionMonitor(withDescription: "Location Dialog") { (alert) -> Bool in
-            alert.buttons["Allow"].tap()
-            return true
-        }
-    
-        let qualAMinhaLocalizaOButton = app.buttons["Qual a minha localização?"]
+        let qualAMinhaLocalizaOButton = XCUIApplication().buttons["Qual a minha localização?"]
         qualAMinhaLocalizaOButton.tap()
-        app.tap()
-        if(!app.buttons["Marcar minha localização"].exists){
-            qualAMinhaLocalizaOButton.tap()
-        }
-//        waitForExpectations(timeout: 20, handler: nil)
-        
-//        snapshot("Localização Achada")
-        
-//        app.buttons["Marcar minha localização"].tap()
-//        snapshot("Tela de Marcação")
-//        let textView = app.tables.children(matching: .cell).element(boundBy: 0).children(matching: .textView).element
-//        textView.tap()
-//        textView.typeText("apple store")
-//        app.navigationBars["Marcação"].buttons["Salvar"].tap()
-        
-        let tabBarsQuery = app.tabBars
-        
-        tabBarsQuery.buttons["Localizações"].tap()
-        snapshot("Tela de Localizações")
-        
-        tabBarsQuery.buttons["Mapa"].tap()
-        snapshot("Tela do Mapa")
-        
+        sleep(10)
+        let qualAMinhaLocalizaOPararButton = XCUIApplication().buttons["Parar"]
+        qualAMinhaLocalizaOPararButton.tap()
         
     }
     
